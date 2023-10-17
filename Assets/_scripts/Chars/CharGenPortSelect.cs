99ca9890bfd9a09900a154f3adcd0b890692c1d6
Hasan YILMAZ <hasanhw@gmail.com>
2023-10-17 09:49:14 +0300

    d

--- Assets/_scripts/Chars/CharGenPortSelect.cs
+++ Assets/_scripts/Chars/CharGenPortSelect.cs
@@ -4,74 +4,27 @@ using UnityEngine;
 
 public class CharGenPortSelect : MonoBehaviour
 {
-    [SerializeField] Renderer rend;
-    [SerializeField] LayerMask _layerMask;
-    [Space]
-    [SerializeField] Transform next;
-    [SerializeField] Transform prev;
-
-    List<Texture2D> _portList;
+    [SerializeField] List<Texture2D> _portList;
     int _portIndex = 0;
-    Camera _camera;
     CharManager charGenerator;
-
-    private void Awake()
-    {
-        if (rend == null)
-        {
-            Debug.LogError("Add Quad Materail");
-        }
-
-        _camera = Camera.main;
-    }
 
     void Start()
     {
         charGenerator = GetComponent<CharManager>();
         _portList = charGenerator.portList;
-        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
     }
 
-
-
-    private void Update()
+    public void NextTexture()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, _layerMask))
-            {
-                if (hit.collider != null)
-                {
-                    if (hit.transform == next) NextTexture();
-                    else if (hit.transform == prev) PrevTexture();
-                }
-            }
-        }
-    }
-
-
-    void ChangeMat(Texture2D texture2D)
-    {
-        rend.material.mainTexture = texture2D;
+        _portIndex++;
+        if (_portIndex >= _portList.Count) _portIndex = 0;
         CharManager.ins.PortIndex = _portIndex;
     }
 
-
-
-    [ContextMenu("NextPort")]
-    void NextTexture()
-    {
-        _portIndex++;
-        if (_portIndex >= _portList.Count) { _portIndex = 0; }
-        ChangeMat(_portList[_portIndex]);
-
-    }
-
-    [ContextMenu("PrevPort")]
-    void PrevTexture()
+    public void PrevTexture()
     {
         _portIndex--;
-        if (_portIndex < 0) { _portIndex = _portList.Count - 1; }
-        ChangeMat(_portList[_portIndex]);
+        if (_portIndex < 0) _portIndex = _portList.Count - 1;
+        CharManager.ins.PortIndex = _portIndex;
     }
 }
